bb6d5d6ce904be7e9888b50da0cfa5cbaf9a8db0
Sujith Nalla <nallasujith1024@gmail.com>
2022-08-08 12:18:44 +0530

    added percentage of loss and profit prediction in app

--- script.js
+++ script.js
@@ -7,13 +7,19 @@ var clickhandler=()=>{
   let initprice=Number(initialpriceref.value);
   let qntyofstocks=Number(qnty_stocks.value);
   let currentprice=Number(cur_price.value);
+  let diffamount=Math.abs(initprice-currentprice);
+  let percentage=Math.trunc(((diffamount)/initprice)*(100));
   if(currentprice > initprice)
   {
-    output_div.innerText="We Made a profit of rupees "+((currentprice-initprice)*(qntyofstocks));
+    output_div.innerText="We Made a profit of rupees "+((currentprice-initprice)*(qntyofstocks)+" with profit percentage "+percentage+"%");
+  }
+  else if(currentprice==initprice)
+  {
+    output_div.innerText="No Profit and No Loss both current price and initial price are same!!";
   }
   else
   {
-    output_div.innerText="We Made a loss of rupees "+((initprice-currentprice)*(qntyofstocks));
+    output_div.innerText="We Made a loss of rupees "+((initprice-currentprice)*(qntyofstocks)+" with loss percentage "+percentage)+"%";
   }
 }
 button.addEventListener("click",clickhandler);
